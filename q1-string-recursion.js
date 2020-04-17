45ea237904d5ebe7502a500bd02c268b3a51bfaf
dustatron <dustymccord@gmail.com>
2020-04-17 08:42:30 -0700

    inital commit.

---
+++ q1-string-recursion.js
@@ -0,0 +1,20 @@
+// Input: "Jasmine Ann Jones"
+// Output: "Jasmine%20Ann%Jones"
+
+const cleanThisString = (stringToClean, index = 0, cleanString = "") => {
+    if(index >= stringToClean.length)
+    {
+        return cleanString;
+    }
+
+    if(stringToClean[index] === " ")
+    {
+        cleanString += "%20";
+    } else {
+        cleanString += stringToClean[index];
+    }
+    return cleanThisString(stringToClean, (index +1), cleanString);
+}
+
+console.log("Running. . .");
+console.log(cleanThisString("Jasmine Ann Jones"));
---
+++ q1-strings.js
@@ -0,0 +1,25 @@
+// URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
+
+// You may not use the replace() method or regular expressions to solve this problem. Solve the problem with and without recursion.
+
+// Example
+// Input: "Jasmine Ann Jones"
+// Output: "Jasmine%20Ann%Jones"
+
+const cleanString = (urlToClear) => {
+    let result = urlToClear
+        .split("")
+        .map((letter) => {
+            if(letter === " ") {
+                return "%20";
+            } else {
+                return letter;
+            }
+        }).join("");
+        
+
+    return result;
+}
+
+console.log("running");
+console.log(cleanString("Jasmine Ann Jones"));
